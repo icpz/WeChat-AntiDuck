240907e188d638808a2308cbb2c6c4e9ef1c63c0
icpz <cc@icpz.dev>
2021-01-22 14:36:53 +0800

    use fishhook instead of funchook

--- src/antiduck.c
+++ src/antiduck.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <CoreAudio/AudioHardware.h>
 
-#include <funchook.h>
+#include <fishhook.h>
 
 #define _STR(x) #x
 #define STR(x) _STR(x)
 
 #define _LOGAT __FILE__ ":" STR(__LINE__) "] "
 #define LOG(fmt, ...) fprintf(stdout, _LOGAT fmt "\n", ##__VA_ARGS__)
-
-OSStatus AudioDeviceDuck(AudioDeviceID inDevice,
-                         Float32 inDuckedLevel,
-                         const AudioTimeStamp* __nullable inStartTime,
-                         Float32 inRampDuration) __attribute__((weak_import));
 
 OSStatus Patched_AudioDeviceDuck(AudioDeviceID inDevice,
                                  Float32 inDucedLevel,
@@ -24,27 +19,22 @@ OSStatus Patched_AudioDeviceDuck(AudioDeviceID inDevice,
 
 __attribute__((constructor))
 static void ctor(void) {
-    funchook_t *hook = funchook_create();
     int ret;
+    void *origin_binding;
 
-    void *add = AudioDeviceDuck;
-    ret = funchook_prepare(hook, &add, Patched_AudioDeviceDuck);
-    if (ret != 0) {
-        LOG("failed to prepare function %d: %s", ret, funchook_error_message(hook));
-        goto __error;
+    struct rebinding rebindingds[] = {
+        { "AudioDeviceDuck", (void *)&Patched_AudioDeviceDuck, &origin_binding},
+    };
+
+    ret = rebind_symbols(rebindingds, 1);
+
+    if (ret) {
+        LOG("rebind failed: %d", ret);
+        return;
     }
 
-    ret = funchook_install(hook, 0);
-    if (ret != 0) {
-        LOG("failed to install function %d: %s", ret, funchook_error_message(hook));
-        goto __error;
-    }
+    LOG("rebind %s with %p, orig=%p", rebindingds[0].name, rebindingds[0].replacement, rebindingds[0].replaced);
 
     LOG("AudioDeviceDuck() patched!");
-
-    return;
-
-__error:
-    funchook_destroy(hook);
 }
 
